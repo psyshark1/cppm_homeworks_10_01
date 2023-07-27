b3a8bb9f66a3aa7fa1381ffa4dec05379a798e70
Andrey <andrey.zalozhenkov@pikta.ru>
2023-07-27 16:11:32 +0400

    Добавьте файлы проекта.

---
+++ 01.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{	
+	std::system("chcp 1251");
+	std::string name;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << name << std::endl;
+	std::system("pause");
+	return 0;
+}
